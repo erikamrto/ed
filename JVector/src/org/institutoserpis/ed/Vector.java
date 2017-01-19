453259b4ebbd670940d9dda46c48d4d11ea3b109
erikamrto <dam.emartorell@institutoserpis.org>
2017-01-19 12:54:47 +0100

    Vector: añadido selectionSort

--- JVector/src/org/institutoserpis/ed/Vector.java
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -25,14 +25,14 @@ public class Vector {
 	}
 
 	public static int indexOf(int[] v, int data) {
-		//	Wirth Algoritmos y Estructuras de datos
-//		int index = 0;
-//		while (index < v.length && v[index] != data)
-//			index++;
-//		if (index == v.length)
-//			return -1;
-//		return index;
-		
+		// Wirth Algoritmos y Estructuras de datos
+		// int index = 0;
+		// while (index < v.length && v[index] != data)
+		// index++;
+		// if (index == v.length)
+		// return -1;
+		// return index;
+
 		int index = 0;
 		for (int item : v) {
 			if (item == data)
@@ -41,16 +41,49 @@ public class Vector {
 		}
 		return -1;
 	}
-	
-	public static int min(int[] v){
+
+	public static int min(int[] v) {
 		int min = v[0];
-//		for (int index = 1; index <v.length; index++)
-//			if (v[index] < min)
-//				min = v[index];
-//		return min;	
-		for (int item :v)
+		// for (int index = 1; index <v.length; index++)
+		// if (v[index] < min)
+		// min = v[index];
+		// return min;
+		for (int item : v)
 			if (item < min)
 				min = item;
 		return min;
+	}
+
+	public static void selectionSortUgly(int[] v) {
+		for (int itemIndex = 0; itemIndex < v.length - 1; itemIndex++) {
+			int indexOfMin = itemIndex;
+			for (int index = itemIndex + 1; index < v.length; index++)
+				if (v[index] < v[indexOfMin])
+					indexOfMin = index;
+			int aux = v[itemIndex];
+			v[itemIndex] = v[indexOfMin];
+			v[indexOfMin] = aux;
+		}
+	}
+	
+	private static int indexOfMin(int[] v, int initialIndex){
+		int indexOfMin = initialIndex;
+		for (int index = initialIndex + 1; index < v.length; index++)
+			if (v[index] < v[indexOfMin])
+				indexOfMin = index;
+		return indexOfMin;
+	}
+	
+	private static void swap(int[]v, int indexOne, int indexTwo) {
+		int aux = v[indexOne];
+		v[indexOne] = v[indexTwo];
+		v[indexTwo] = aux;
+	}
+	
+	public static void selectionSort(int[] v) {
+		for (int itemIndex = 0; itemIndex < v.length - 1; itemIndex++) {
+			int indexOfMin =  indexOfMin(v, itemIndex);
+			swap(v, itemIndex, indexOfMin);
+		}
 	}
 }
